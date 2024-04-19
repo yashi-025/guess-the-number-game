1005d9b379acad4ab389bb8e2f1dc822c01a324e
yashi-025 <146928623+yashi-025@users.noreply.github.com>
2024-04-20 01:32:42 +0530

    Add files via upload

---
+++ guessgame.py
@@ -0,0 +1,24 @@
+import random
+min=int(input("enter the lower limit:"))
+max=int(input("enter the highest limit:"))
+num=random.randint(min,max)
+guess=None
+count=0
+while guess!=num:
+    guess=int(input("guess the number:"))
+    if guess==num:
+        print("wohoo!! you won!!")
+        count+=1
+        print("total attempts taken:",count)
+        if count<=5:
+            print("superb gammer!! :)")
+        else:
+            print("good try")    
+        break
+    elif guess>num:
+        print("too high!")
+        count+=1
+    elif guess<num:
+        print("too low!") 
+        count+=1   
+    
